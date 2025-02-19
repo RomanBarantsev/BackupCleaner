c3e269b72e46d28c61c6bfb5a973e0766f9d8a71
Roman <roman.it.kirov@yandex.ru>
2025-02-19 23:08:24 +0300

    temporary commit
    adding class for dynamic creating lists of folders.

--- BackupCleaner/BackupCleaner/MainWindow.h
+++ BackupCleaner/BackupCleaner/MainWindow.h
@@ -12,6 +12,47 @@ namespace BackupCleaner {
 	/// <summary>
 	/// Summary for MainWindow
 	/// </summary>
+	/// 
+	public ref class Folder : public System::Windows::Forms::Form {
+	private:
+		System::Windows::Forms::GroupBox^ groupBox1;
+		System::Windows::Forms::Button^ button1;
+		Folder(System::Windows::Forms::GroupBox^ groupBox) {
+			this->groupBox1->Controls->Add(groupBox);
+		}
+#pragma region Windows Form Designer generated code
+		void InitializeComponent(void) {
+			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->groupBox1->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// groupBox1
+			// 
+			this->groupBox1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->groupBox1->Location = System::Drawing::Point(0, 0);
+			this->groupBox1->Name = L"groupBox1";
+			this->groupBox1->Size = System::Drawing::Size(1375, 729);
+			this->groupBox1->TabIndex = 0;
+			this->groupBox1->TabStop = false;
+			this->groupBox1->Text = L"groupBox1";
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(1250, 22);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(94, 23);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Folder::button1_Click);
+		}
+#pragma endregion 
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+
+	}
+
+	};
 	public ref class MainWindow : public System::Windows::Forms::Form
 	{
 	public:
@@ -34,12 +75,41 @@ namespace BackupCleaner {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::GroupBox^ groupBox1;
+	private: System::Windows::Forms::GroupBox^ groupBox2;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	protected:
+
+	protected:
+
+	protected:
+
+
+
+	protected:
+
+	protected:
+
+	protected:
+
+
+
+
+
+
+
+
+
+
+
+	private: System::ComponentModel::IContainer^ components;
 
 	private:
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -48,12 +118,70 @@ namespace BackupCleaner {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MainWindow";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+			this->groupBox2 = (gcnew System::Windows::Forms::GroupBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->groupBox1->SuspendLayout();
+			this->groupBox2->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// groupBox1
+			// 
+			this->groupBox1->Controls->Add(this->groupBox2);
+			this->groupBox1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->groupBox1->Location = System::Drawing::Point(0, 0);
+			this->groupBox1->Name = L"groupBox1";
+			this->groupBox1->Size = System::Drawing::Size(1375, 729);
+			this->groupBox1->TabIndex = 0;
+			this->groupBox1->TabStop = false;
+			this->groupBox1->Text = L"groupBox1";
+			// 
+			// groupBox2
+			// 
+			this->groupBox2->Controls->Add(this->textBox1);
+			this->groupBox2->Controls->Add(this->button1);
+			this->groupBox2->Location = System::Drawing::Point(11, 21);
+			this->groupBox2->Name = L"groupBox2";
+			this->groupBox2->Size = System::Drawing::Size(1350, 62);
+			this->groupBox2->TabIndex = 1;
+			this->groupBox2->TabStop = false;
+			this->groupBox2->Text = L"groupBox2";
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(1250, 22);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(94, 23);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MainWindow::button1_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(1144, 21);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(100, 22);
+			this->textBox1->TabIndex = 1;
+			// 
+			// MainWindow
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1375, 729);
+			this->Controls->Add(this->groupBox1);
+			this->Name = L"MainWindow";
+			this->Text = L"MainWindow";
+			this->groupBox1->ResumeLayout(false);
+			this->groupBox2->ResumeLayout(false);
+			this->groupBox2->PerformLayout();
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
-	};
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		
+	}
+};
 }
