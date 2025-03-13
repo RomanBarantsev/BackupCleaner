18fb8c531cad52f85327908416ce5d9b5aa3636f
Roman <roman.it.kirov@yandex.ru>
2025-03-13 12:01:43 +0300

    Fixing tooltip text, it's static, so no reason to store in class values.

--- BackupCleaner/BackupCleaner/FolderData.h
+++ BackupCleaner/BackupCleaner/FolderData.h
@@ -6,14 +6,10 @@ struct FolderData
 {
 public:
 	int daysToStore;
-	std::string daysToStoreTooltip;
 	int folderSize;
-	std::string folderSizeTooltip;
 	int countFiles;
-	std::string countFilesTooltip;
-	FolderData(int Days, std::string DaysTooltip, int Size, std::string SizeTooltip, int count, std::string countTooltip) :
-		daysToStore(Days), daysToStoreTooltip(DaysTooltip), folderSize(Size),
-		folderSizeTooltip(SizeTooltip), countFiles(count), countFilesTooltip(countTooltip) {
+	FolderData(int Days, int Size, int count) :
+		daysToStore(Days), folderSize(Size), countFiles(count) {
 	}
 };
 
