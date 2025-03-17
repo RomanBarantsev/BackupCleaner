fc8d8641283112630f2f3b2e48fbe5a32e0cef71
Roman <roman.it.kirov@yandex.ru>
2025-03-17 17:50:04 +0300

    Update FolderData.h
    deleting heap memory storage. Not necessary.

--- BackupCleaner/BackupCleaner/FolderData.h
+++ BackupCleaner/BackupCleaner/FolderData.h
@@ -5,21 +5,16 @@
 struct FolderData
 {
 public:
-	int* daysToStore;
-	int* folderSize;
-	int* countFiles;
+	int daysToStore;
+	int folderSize;
+	int countFiles;
 	FolderData(int Days, int Size, int count) {
-		daysToStore = new int(Days);
-		folderSize = new int(Size);
-		countFiles = new int(count);
+		daysToStore = Days;
+		folderSize = Size;
+		countFiles = count;
 	}
 	~FolderData() {
-		delete daysToStore;
-		delete folderSize;
-		delete countFiles;
-		daysToStore = nullptr;
-		folderSize = nullptr;
-		countFiles = nullptr;
+
 	}
 };
 
