4562be833cb15fb01bb3c0c9eeedb23786f1210d
Roman <roman.it.kirov@yandex.ru>
2025-04-02 16:43:57 +0300

    Update MainWindow.cpp
    creating scheduler task on start,
    if starts with argument "1" it's come from Windows scheduler which means that we should start cleaning procedure.

--- BackupCleaner/BackupCleaner/MainWindow.cpp
+++ BackupCleaner/BackupCleaner/MainWindow.cpp
@@ -1,12 +1,23 @@
 #include "MainWindow.h"
 #include <Windows.h>
+#include "Cleaner.h"
 
 using namespace BackupCleaner;
 
 [STAThread]
-int WINAPI WinMain(HINSTANCE ,HINSTANCE, LPSTR, int) {
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
+    CreateScheduledTask();    
+    std::wstring arg = pCmdLine;
+    if (arg == L"1") {
+        StoreData storeDt = StoreData();
+        auto data = storeDt.GetData();
+        for (const auto& [first, second] : data) {
+            Cleaner cl(first, second->daysToStore, second->countFiles, second->folderSize);
+        }
+        return 0;
+    }
     Application::SetCompatibleTextRenderingDefault(false);
     Application::EnableVisualStyles();
-    Application::Run(gcnew MainWindow());
+    Application::Run(gcnew MainWindow());    
     return 0;
 }
